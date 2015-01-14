8123d603c8d50926133aecef76d9370d8a317f5a
James Wright <james@jamesswright.co.uk>
2015-01-14 23:10:25 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+var quotes = require('./quotes');
+var app = express();
+var port = process.env.SWANSON_PORT || 3000;
+
+app.get('/quotes', function (req, res) {
+	var quote = quotes[Math.floor(Math.random() * quotes.length)];
+	res.json({quote: quote});
+});
+
+app.listen(port, function () {
+	console.log('Server running on port', port);
+})
