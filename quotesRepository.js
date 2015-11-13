8eaac4806e2aa256701f499792089b05d7ced5f1
James Wright <james.wright@net-a-porter.com>
2015-11-13 18:04:43 +0000

    Better array perf, ensuring that exact number of requested quotes are returned if below total available

--- quotesRepository.js
+++ quotesRepository.js
@@ -3,17 +3,23 @@
 var quotes = require('./quotes');
 
 module.exports = {
-	getRandom: function getRandom(num) {
-		var out = [];
+	getRandom: function getRandom(numberOfQuotes) {
+		var hasRequestedTooMany = numberOfQuotes > quotes.length;
+		var limit =  hasRequestedTooMany ? quotes.length : numberOfQuotes;
+		var out = new Array(limit);
+		var quote;
+		var quoteAlreadyUsed;
 
-		while (num > 0) {
-			var quote = quotes[Math.floor(Math.random() * quotes.length)];
-			var quoteAlreadyUsed = out.indexOf(quote) > -1;
+		for (var i = 0; i < limit; i++) {
+			quote = quotes[Math.floor(Math.random() * quotes.length)];
+			quoteAlreadyUsed = out.indexOf(quote) > -1;
 
-			if (!quoteAlreadyUsed)
-		 		out.push(quote);
-		 	
-		 	num--;
+			while (quoteAlreadyUsed) {
+				quote = quotes[Math.floor(Math.random() * quotes.length)];
+				quoteAlreadyUsed = out.indexOf(quote) > -1;
+			}
+
+			out[i] = quote;
 		}
 
 		return out;
